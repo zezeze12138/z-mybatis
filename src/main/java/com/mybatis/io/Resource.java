7dcfb57fe687fb59b501d043727b0d404f693f0e
zengqz <954723922@qq.com>
2022-07-30 23:18:59 +0800

    test

--- src/main/java/com/mybatis/io/Resource.java
+++ src/main/java/com/mybatis/io/Resource.java
@@ -9,7 +9,7 @@ public class Resource {
 
     /**
      * 通过资源文件路径，获取配置资源的方法
-     * @param resource 资源文件路径
+     * @param resource 资源文件路径s
      * @return
      */
     public static InputStream getResourceAsStream(String resource){
