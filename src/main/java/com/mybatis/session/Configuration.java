e76bc5bab41864c80945a12b6249a8d9fbd32fd4
zengqz <954723922@qq,com>
2022-08-02 21:00:18 +0800

    添加创建执行器中的拦截链插件处理注释

--- src/main/java/com/mybatis/session/Configuration.java
+++ src/main/java/com/mybatis/session/Configuration.java
@@ -2,6 +2,7 @@ package com.mybatis.session;
 
 import com.mybatis.binding.MapperRegistry;
 import com.mybatis.executor.Executor;
+import com.mybatis.executor.SimpleExecutor;
 import com.mybatis.mapping.MappedStatement;
 
 import java.util.HashMap;
@@ -41,7 +42,11 @@ public class Configuration {
      */
     public Executor newExecutor(ExecutorType executorType){
         Executor executor = null;
-
+        if(executorType.SIMPLE == executorType){
+            executor = new SimpleExecutor();
+        }
+        // TODO: 2022/8/2  源码中还有一个拦截链，通过拦截链往创建的执行器中添加插件处理
+        //这里暂时线不做实现拦截链的实现
         return executor;
     }
 }
