03376d050d8ac309c365486cfbefcd9268d92a68
zengqz <954723922@qq.com>
2022-08-08 00:28:03 +0800

    无连接池的数据源增加驱动代理静态内部类

--- src/main/java/com/mybatis/datasource/unpooled/UnpooledDataSource.java
+++ src/main/java/com/mybatis/datasource/unpooled/UnpooledDataSource.java
@@ -92,7 +92,7 @@ public class UnpooledDataSource implements DataSource {
                 //生成驱动实例
                 Driver driverInstance = (Driver) driverType.getDeclaredConstructor().newInstance();
                 //注册驱动实例
-                DriverManager.registerDriver(driverInstance);
+                DriverManager.registerDriver(new DriverProxy(driverInstance));
                 registeredDrivers.put(driver, driverInstance);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -166,5 +166,51 @@ public class UnpooledDataSource implements DataSource {
     @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         return Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    }
+
+    /**
+     * 驱动代理类
+     */
+    private static class DriverProxy implements Driver{
+        private Driver driver;
+
+        public DriverProxy(Driver driver) {
+            this.driver = driver;
+        }
+
+        @Override
+        public Connection connect(String url, Properties info) throws SQLException {
+            return this.driver.connect(url, info);
+        }
+
+        @Override
+        public boolean acceptsURL(String url) throws SQLException {
+            return this.driver.acceptsURL(url);
+        }
+
+        @Override
+        public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+            return this.driver.getPropertyInfo(url, info);
+        }
+
+        @Override
+        public int getMajorVersion() {
+            return this.driver.getMajorVersion();
+        }
+
+        @Override
+        public int getMinorVersion() {
+            return this.driver.getMinorVersion();
+        }
+
+        @Override
+        public boolean jdbcCompliant() {
+            return this.driver.jdbcCompliant();
+        }
+
+        @Override
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+            return Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+        }
     }
 }
